53f6d953f31f6496c266776a9c619973c0aaf092
ArisSpn_9 <arissopian0909@gmail.com>
2026-01-27 22:58:55 +0700

    add: routes to each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Projects from './Pages/Projects'
+import Contact from './Pages/Contact'
 import Footer from './components/Footer'
 
 import { ThemeProvider } from './context/ThemeContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="bg-light-bg dark:bg-dark-bg min-h-screen transition-colors duration-300">
-        <Navbar />
-        <Hero />
-        <About />
-        <Projects />
-        <Contact />
-        <Footer />
-      </div>
+      <Router>
+        <div className="bg-light-bg dark:bg-dark-bg min-h-screen transition-colors duration-300">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
     </ThemeProvider>
   )
 }

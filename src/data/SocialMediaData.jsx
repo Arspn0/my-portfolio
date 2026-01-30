4b7ae9567e207755e1c022254900ba618a6008b8
ArisSpn_9 <arissopian0909@gmail.com>
2026-01-30 20:19:26 +0700

    add: social media data

---
+++ src/data/SocialMediaData.jsx
@@ -0,0 +1,22 @@
+import { Github, Linkedin, Instagram } from 'lucide-react'
+
+export const socialMediaLinks = [
+    { 
+        icon: Github, 
+        href: 'https://github.com/Arspn0',
+        label: 'GitHub',
+        color: 'neo-cyan'
+    },
+    { 
+        icon: Linkedin, 
+        href: 'https://www.linkedin.com/in/aris-sopian-9520a2246',
+        label: 'LinkedIn',
+        color: 'neo-pink'
+    },
+    { 
+        icon: Instagram, 
+        href: 'https://www.instagram.com/aaariss_9/',
+        label: 'Instagram',
+        color: 'neo-yellow'
+    }
+]

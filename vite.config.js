77fa1a117ec2d3f5874a20dbd378b8f608f1866b
ArisSpn_9 <arissopian0909@gmail.com>
2026-02-07 23:46:20 +0700

    update: path to redirects

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: 'public/_redirects', dest: 'dist' }
+      ],
+      hook: 'writeBundle'
+    })
+  ],
   build: {
     outDir: 'dist',
   },
-  publicDir: 'public',
 })

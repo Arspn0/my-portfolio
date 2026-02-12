d3d1ed3eb4d03b1f4f89fc6b9ec3109470b8ddfb
ArisSpn_9 <arissopian0909@gmail.com>
2026-02-13 01:25:01 +0700

    update: config for netlify

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    copy({
-      targets: [
-        { src: 'public/_redirects', dest: 'dist' }
-      ],
-      hook: 'writeBundle'
-    })
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
   },
-  base: '/',
 })
